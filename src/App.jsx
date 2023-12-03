f5d666d40f795715402aef685225c479788137f3
Bwa <40144023+beaesq@users.noreply.github.com>
2023-12-03 21:24:59 +0800

    Add pokemon card reset button

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
 import { useEffect, useState } from "react";
-import "./App.css";
+import "./styles/App.css";
 import getList from "./pokeapi";
 import CardContainer from "./components/CardContainer";
 
 function App() {
   const [list, setList] = useState([]);
   const [score, setScore] = useState(0);
+  const [bestScore, setbestScore] = useState(0);
+  const [resetCount, setResetCount] = useState(0);
 
   useEffect(() => {
     getList().then((response) => {
       setList(response);
     });
-  }, []);
+  }, [resetCount]);
 
   function increaseScore() {
     setScore(score + 1);
+    if (score + 1 > bestScore) {
+      setbestScore(score + 1);
+    }
   }
 
   function resetScore() {
     setScore(0);
   }
 
+  function handleResetClick() {
+    setResetCount(resetCount + 1);
+    setScore(0);
+  }
+
   return (
-    <div>
-      <p>Score: {score}</p>
+    <>
+      <header>
+        <h1>Generation I Pokémon Memory Game</h1>
+        <p className="score">Score: {score}</p>
+        <p className="high-score">Best Score: {bestScore}</p>
+        <button type="button" onClick={handleResetClick}>
+          Reset Pokémon
+        </button>
+      </header>
       <CardContainer
         list={list}
         increaseScore={() => increaseScore()}
         resetScore={() => resetScore()}
       />
-    </div>
+    </>
   );
 }
 
